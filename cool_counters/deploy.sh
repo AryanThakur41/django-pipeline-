fa2a8d41edaf091ca44f69d81ec1f9e715737004
Aryan Thakur <149156092+AryanThakur41@users.noreply.github.com>
2024-09-07 14:26:22 +0530

    Update deploy.sh

--- cool_counters/deploy.sh
+++ cool_counters/deploy.sh
@@ -2,7 +2,7 @@
 
 # Clone the repository only if the directory doesn't exist
 #if [ ! -d "/home/ubuntu/django-pipeline-" ]; then
-    git clone https://github.com/AryanThakur41/portfolio.git /home/ubuntu/
+    git clone https://github.com/AryanThakur41/portfolio.git 
 #else
 #    echo "Directory already exists, skipping clone."
 #fi
