772159edb8770ec21b3033063865b73e49c0e6ae
Aryan Thakur <149156092+AryanThakur41@users.noreply.github.com>
2024-09-07 12:30:48 +0530

    Update models.py

--- cool_counters/counter/models.py
+++ cool_counters/counter/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
 class Counter(models.Model):
+    id = models.BigAutoField(primary_key=True)
     key = models.CharField(max_length=10)
     value = models.IntegerField() 
