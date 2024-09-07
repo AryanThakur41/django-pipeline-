f5d6a6ee946e12c5f9becbc0e4985c14990467fb
Aryan Thakur <149156092+AryanThakur41@users.noreply.github.com>
2024-09-07 12:18:42 +0530

    Update deploy.sh

--- cool_counters/deploy.sh
+++ cool_counters/deploy.sh
@@ -24,11 +24,11 @@ source venv/bin/activate
 pip install -r ./cool_counters/requirements.txt
 
 # Collect static files
-python3 manage.py collectstatic --noinput
+python3 ./cool_counters/manage.py collectstatic --noinput
 
 # Apply migrations
-python3 manage.py migrate
+python3 ./cool_counters/manage.py migrate
 
 # Restart Gunicorn or Django server
 # sudo systemctl restart gunicorn
-python3 manage.py runserver
+python3 ./cool_counters/manage.py runserver
